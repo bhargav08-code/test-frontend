48bf3062cb0c99a2de34e54d68a3121bb983d91c
bhargav08-code <85948072+bhargav08-code@users.noreply.github.com>
2024-06-15 14:45:14 +0530

    Update Verify.jsx

--- src/pages/Verify/Verify.jsx
+++ src/pages/Verify/Verify.jsx
@@ -5,25 +5,33 @@ import { StoreContext } from "../../Context/StoreContext";
 import axios from "axios";
 
 const Verify = () => {
-  const [searchParam, setSearchParam] = useSearchParams();
+  const [searchParam] = useSearchParams();
   const success = searchParam.get("success");
   const orderId = searchParam.get("orderId");
   const { url } = useContext(StoreContext);
   const navigate = useNavigate();
+
   const verifyPayment = async () => {
-    const res = await axios.post(url + "/api/order/verify", {
-      success,
-      orderId,
-    });
-    if (res.data.success) {
-      navigate("/myorders");
-    } else {
+    try {
+      const res = await axios.post(url + "/api/order/verify", {
+        success,
+        orderId,
+      });
+      if (res.data.success) {
+        navigate("/myorders");
+      } else {
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("Error verifying payment:", error);
       navigate("/");
     }
   };
+
   useEffect(() => {
     verifyPayment();
-  }, []);
+  }, []); // Empty dependency array ensures this runs only once after the initial render
+
   return (
     <div className="verify">
       <div className="spinner"></div>
